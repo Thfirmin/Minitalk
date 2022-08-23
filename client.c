8b3f2c59fafe60cf258c48943ef0f5ecc3b4cfde
thfirmin <thiagofirmino2001@gmail.com>
2022-08-23 02:13:44 -0300

    finalizing confirmation of sended message for server implementation

--- client.c
+++ client.c
@@ -6,11 +6,13 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 21:54:54 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/08/23 01:08:16 by Thinotsuki   ###     ###.br              */
+/*   Updated: 2022/08/23 02:11:57 by Thinotsuki   ###     ###.br              */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	ft_return(int signum);
 
 int	ft_isalldigit(char	*str);
 
@@ -18,16 +20,17 @@ int	main(int argc, char *argv[])
 {
 	pid_t	n_pid;
 
+	signal(SIGUSR1, ft_return);
 	if ((argc != 3) || (!ft_isalldigit(argv[1])))
 	{
 		ft_printf("\e[38;5;196mERROR: \e[mincorrect argument!\e[38;5;87m\n");
 		ft_printf("NOTE: \e[mCorrect Argument format [./client <PID> <STR>]\n");
 		exit(EXIT_FAILURE);
 	}
-//	(void)argv[2];
 	n_pid = ft_atoi(argv[1]);
 	kill(n_pid, SIGUSR1);
-	exit(EXIT_SUCCESS);
+	usleep (300);
+	exit(EXIT_FAILURE);
 }
 
 int	ft_isalldigit(char	*str)
@@ -38,5 +41,12 @@ int	ft_isalldigit(char	*str)
 			return (0);
 	}
 	return (1);
+}
+
+void	ft_return(int signum)
+{
+	(void)signum;
+	ft_printf ("Message sucefully from sended!\n");
+	exit(EXIT_SUCCESS);
 }
 
--- server.c
+++ server.c
@@ -6,30 +6,32 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 21:55:15 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/08/23 01:14:22 by Thinotsuki   ###     ###.br              */
+/*   Updated: 2022/08/23 02:03:06 by Thinotsuki   ###     ###.br              */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	ft_usrhandler(int signum);
+void	usrhandler(int signum, siginfo_t *info, void *context);
 
 int	main(void)
 {
-	pid_t	n_pid;
 	struct sigaction usr_h;
-	usr_h.sa_handler = ft_usrhandler;
-	usr_h.sa_flags = SA_RESTART;
 
-	n_pid = getpid();
+	usr_h.sa_sigaction = &usrhandler;
+	usr_h.sa_flags = SA_SIGINFO | SA_RESTART;
+
 	sigaction (SIGUSR1, &usr_h, NULL);
 	ft_printf("\e[38;5;112;1mStarted Process!\e[m\n");
-	ft_printf ("PID: \e[38;5;112;1m%d\e[m\n", n_pid);
+	ft_printf ("PID: \e[38;5;112;1m%d\e[m\n", getpid());
 	while (1)
 		pause();
+	return (1);
 }
 
-void	ft_usrhandler(int signum)
+void	usrhandler(int signum, siginfo_t *info, void *context)
 {
+	(void)context;
 	ft_printf("SIGNAL FROM %d RECEIVED!\n", signum);
+	kill(info->si_pid, SIGUSR1);
 }
