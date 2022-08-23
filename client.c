c93dad16d8d8438d7f1f6a51d184e639aa367469
thfirmin <thiagofirmino2001@gmail.com>
2022-08-23 00:27:10 -0300

    Sending a custom signal and receiving from server with a message

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 21:54:54 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/08/23 00:18:58 by Thinotsuki   ###     ###.br              */
+/*   Updated: 2022/08/23 00:20:52 by Thinotsuki   ###     ###.br              */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ int	main(int argc, char *argv[])
 	pid_t	n_pid;
 
 	n_pid = ft_atoi(argv[(argc - 1)]);
-	kill(n_pid, SIGHUP);
+	kill(n_pid, SIGUSR1);
 }
--- server.c
+++ server.c
@@ -6,19 +6,27 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 21:55:15 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/08/23 00:13:15 by Thinotsuki   ###     ###.br              */
+/*   Updated: 2022/08/23 00:25:11 by Thinotsuki   ###     ###.br              */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	ft_handler(int signum);
 
 int	main(void)
 {
 	pid_t	n_pid;
 
 	n_pid = getpid();
+	signal (SIGUSR1, ft_handler);
 	ft_printf("\e[38;5;112;1mStarted Process!\e[m\n");
 	ft_printf ("PID: \e[38;5;112;1m%d\e[m\n", n_pid);
 	while (1)
 		pause();
+}
+
+void	ft_handler(int signum)
+{
+	ft_printf("SIGNAL FROM %d RECEIVED!\n", signum);
 }
