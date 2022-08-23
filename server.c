69532c12614f639f2d0f7a418325f61c48d2c637
thfirmin <thiagofirmino2001@gmail.com>
2022-08-23 00:12:57 -0300

    add function in program to running in indeterminate time

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 21:55:15 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/08/23 00:04:00 by Thinotsuki   ###     ###.br              */
+/*   Updated: 2022/08/23 00:10:33 by Thinotsuki   ###     ###.br              */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,6 @@ int	main(void)
 	n_pid = getpid();
 	ft_printf("\e[38;5;112;1mStarted Process!\e[m\n");
 	ft_printf ("PID: \e[38;5;112;1m%d\n", n_pid);
+	while (1)
+		pause();
 }
