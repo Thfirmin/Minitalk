1c801e8144ac8e58817578f7a59a1d697f6c7bdb
thfirmin <thiagofirmino2001@gmail.com>
2022-08-23 00:14:16 -0300

    color display bug fix

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 21:55:15 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/08/23 00:10:33 by Thinotsuki   ###     ###.br              */
+/*   Updated: 2022/08/23 00:13:15 by Thinotsuki   ###     ###.br              */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(void)
 
 	n_pid = getpid();
 	ft_printf("\e[38;5;112;1mStarted Process!\e[m\n");
-	ft_printf ("PID: \e[38;5;112;1m%d\n", n_pid);
+	ft_printf ("PID: \e[38;5;112;1m%d\e[m\n", n_pid);
 	while (1)
 		pause();
 }
