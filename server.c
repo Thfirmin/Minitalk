eada05d8b7e212f75bbf351e623f678bd82b8c1f
thfirmin <thiagofirmino2001@gmail.com>
2022-08-23 02:58:46 -0300

    printing binarys received

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: thfirmin <thfirmin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 21:55:15 by thfirmin          #+#    #+#             */
-/*   Updated: 2022/08/23 02:03:06 by Thinotsuki   ###     ###.br              */
+/*   Updated: 2022/08/23 02:58:35 by Thinotsuki   ###     ###.br              */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ int	main(void)
 
 	usr_h.sa_sigaction = &usrhandler;
 	usr_h.sa_flags = SA_SIGINFO | SA_RESTART;
-
 	sigaction (SIGUSR1, &usr_h, NULL);
+	sigaction (SIGUSR2, &usr_h, NULL);
 	ft_printf("\e[38;5;112;1mStarted Process!\e[m\n");
 	ft_printf ("PID: \e[38;5;112;1m%d\e[m\n", getpid());
 	while (1)
@@ -31,7 +31,14 @@ int	main(void)
 
 void	usrhandler(int signum, siginfo_t *info, void *context)
 {
+//	static int	bits;
+
 	(void)context;
-	ft_printf("SIGNAL FROM %d RECEIVED!\n", signum);
-	kill(info->si_pid, SIGUSR1);
+	(void)info;
+	if (signum == SIGUSR2)
+		ft_putchar_fd('1', 1);
+	else
+		ft_putchar_fd('0', 1);
+//	if (c == 0)
+i//		kill(info->si_pid, SIGUSR1);
 }
